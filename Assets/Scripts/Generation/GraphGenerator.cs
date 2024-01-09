8fb636c89f2809bd02873ac11d2c417782bbd721
MrEsport <lucasespositonc@gmail.com>
2024-01-09 11:08:15 +0100

    [EDIT] clamp graph size
    Co-Authored-By: Luca Schifano <70594490+LucaSCHIFANO@users.noreply.github.com>

--- Assets/Scripts/Generation/GraphGenerator.cs
+++ Assets/Scripts/Generation/GraphGenerator.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class GraphGenerator : MonoBehaviour
 {
+    [SerializeField] private int minRoomNumber;
     [SerializeField] private int maxRoomNumber;
+
+    private int doorCount;
+    private int roomCount;
 
     void Start()
     {
@@ -14,17 +18,43 @@ public class GraphGenerator : MonoBehaviour
     private void GenerateDungeon()
     {
         Node startNode = new Node(1);
+        doorCount = 1;
+        roomCount = 0;
         GenerateRoom(startNode);
+
+        Debug.Log($"Generated {roomCount} rooms, with {doorCount} doors");
     }
 
     private void GenerateRoom(Node parentNode)
     {
         string childs = "";
+        roomCount++;
+        doorCount += parentNode.DoorCount - 1;
 
         while (parentNode.DoorCount > parentNode.Connections.Count)
         {
-            Node nextNode = new Node(Random.Range(1, 4));
+            int minRandomRange = 1;
+            int maxRandomRange = 5;
+
+            if (doorCount >= maxRoomNumber - 1)
+            {
+                minRandomRange = 1;
+                maxRandomRange = 2;
+            }
+            else if (roomCount < minRoomNumber)
+            {
+                minRandomRange = 2;
+                maxRandomRange = 4;
+            }
+
+            int nextDoorCount = Random.Range(minRandomRange, maxRandomRange);
+            if (doorCount + nextDoorCount >= maxRoomNumber)
+                nextDoorCount = maxRoomNumber - doorCount;
+
+            Node nextNode = new Node(nextDoorCount);
+
             childs += $"{nextNode.NodeId}, ";
+
             parentNode.Connect(nextNode);
             GenerateRoom(nextNode);
         }
